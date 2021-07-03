a3f1e14383920400657f7c8f1fa54121d665a71c
Susil Shah <susil.shah@alphaventus.com>
2021-07-03 21:42:56 +0545

    route issue fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
 import LoginPage from "./Page/Login";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Page/Home";
 
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter>
+        <Route path="/" exact>
+          <LoginPage />
+        </Route>
         <Route path="/home" exact>
           <Home />
         </Route>
-        <LoginPage />
       </BrowserRouter>
     </React.Fragment>
   );
