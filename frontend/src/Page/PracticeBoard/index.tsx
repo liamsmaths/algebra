8c0d79f65b0b2553eb376a0f850d9ecfa1ff418c
Susil Shah <susil.shah@alphaventus.com>
2021-07-07 18:16:51 +0545

    instruction and get help section developed

--- frontend/src/Page/PracticeBoard/index.tsx
+++ frontend/src/Page/PracticeBoard/index.tsx
@@ -5,7 +5,8 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 import Text from "antd/lib/typography/Text";
 import MathJax from "react-mathjax";
-import { Input, Button, Row, Col, Divider } from "antd";
+import { Input, Button, Row, Col, Divider, Form, Popover } from "antd";
+import { useForm } from "antd/lib/form/Form";
 const { TextArea } = Input;
 
 const Header = styled("div")`
@@ -38,7 +39,7 @@ const QuestionContainer = styled("div")`
 `;
 
 const InstructionContainer = styled("div")`
-  padding: 10px 0px 0px 18px;
+  padding: 30px;
   box-shadow: 0 4px 6px rgb(0 0 0 / 10%), 0 1px 3px rgb(0 0 0 / 10%);
   background-color: #fff;
   border-radius: 4px;
@@ -66,6 +67,11 @@ const PracticeBoard = () => {
   const [inputAnswer, setInputAnswer] = useState("");
   const [correct, setCorrect] = useState<number>(0);
   const [totalAttempts, setTotalAttempts] = useState<number>(0);
+  const [help, setHelp] = useState<any>();
+  const [isGetHelp, setIsGetHelp] = useState<boolean>(false);
+  const [correctPopover, setCorrectPopover] = useState<boolean>(false);
+
+  const [form] = useForm();
 
   const fetchAllQuestions = async () => {
     try {
@@ -78,13 +84,27 @@ const PracticeBoard = () => {
     fetchAllQuestions();
   }, []);
 
+  const getHelp = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:8000/api/getHelp", {
+        question: allQuestions.data.title,
+        answer: allQuestions.data.answer,
+        effort: inputAnswer,
+        topic: id,
+      });
+
+      setHelp(response.data.get_help);
+      setIsGetHelp(true);
+    } catch (e) {}
+  };
+
   const handleChange = (e: any) => {
     const inputValue = e.target.value;
     setInputAnswer(inputValue);
-    if (inputValue.length >= 6) {
+    if (inputValue.length > 0) {
       setIsDisabled(false);
     }
-    if (inputValue.length < 6) {
+    if (inputValue.length === 0) {
       setIsDisabled(true);
     }
   };
@@ -95,7 +115,13 @@ const PracticeBoard = () => {
     const correctAnswerStringify = allQuestions.data.answer.toString();
 
     if (inputAnswerStringify === correctAnswerStringify) {
+      form.setFieldsValue({ userinput: "" });
+      setCorrectPopover(true);
       setCorrect(correct + 1);
+      setIsGetHelp(false);
+      handleNextQuestion();
+    } else {
+      getHelp();
     }
   };
 
@@ -115,35 +141,45 @@ const PracticeBoard = () => {
           <Row gutter={64}>
             <Col span={16}>
               <QuestionContainer>
-                <Text style={{ fontSize: "20px", fontWeight: 600, paddingRight: "10px" }}>
-                  {name} :
-                </Text>
+                <div style={{ display: "flex", justifyContent: "space-between" }}>
+                  <div>
+                    <Text style={{ fontSize: "20px", fontWeight: 600, paddingRight: "10px" }}>
+                      {name} :
+                    </Text>
 
-                <Text style={{ fontSize: "18px" }}>
-                  <MathJax.Node
-                    inline
-                    formula={allQuestions && allQuestions.data && allQuestions.data.title}
-                  />
-                </Text>
+                    <Text style={{ fontSize: "18px" }}>
+                      <MathJax.Node
+                        inline
+                        formula={allQuestions && allQuestions.data && allQuestions.data.title}
+                      />
+                    </Text>
+                  </div>
+                </div>
 
                 <Divider orientation="left">Answer</Divider>
-                <Row gutter={64}>
-                  <Col xs={24} sm={24} md={10}>
-                    <TextArea rows={4} onChange={handleChange} />
-                  </Col>
-                  <Col xs={24} sm={24} md={7}>
-                    <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
-                      <Text style={{ fontSize: "18px", fontWeight: 600 }}>Correct</Text>
-                      <Text style={{ fontSize: "18px", fontWeight: 600 }}>{correct}</Text>
-                    </div>
-                  </Col>
-                  <Col xs={24} sm={24} md={7}>
-                    <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
-                      <Text style={{ fontSize: "18px", fontWeight: 600 }}>Total Attempts</Text>
-                      <Text style={{ fontSize: "18px", fontWeight: 600 }}>{totalAttempts}</Text>
-                    </div>
-                  </Col>
-                </Row>
+                <Form name="basic" form={form}>
+                  <Row gutter={64}>
+                    <Col xs={24} sm={24} md={10}>
+                      <Form.Item name="userinput">
+                        <TextArea rows={4} onChange={handleChange} />
+                      </Form.Item>
+                    </Col>
+
+                    <Col xs={24} sm={24} md={7}>
+                      <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+                        <Text style={{ fontSize: "18px", fontWeight: 600 }}>Correct</Text>
+                        <Text style={{ fontSize: "18px", fontWeight: 600 }}>{correct}</Text>
+                      </div>
+                    </Col>
+                    <Col xs={24} sm={24} md={7}>
+                      <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
+                        <Text style={{ fontSize: "18px", fontWeight: 600 }}>Total Attempts</Text>
+                        <Text style={{ fontSize: "18px", fontWeight: 600 }}>{totalAttempts}</Text>
+                      </div>
+                    </Col>
+                  </Row>
+                </Form>
+
                 <Row gutter={12} style={{ marginTop: "12px", paddingTop: "15px" }}>
                   <Col xs={24} sm={3}>
                     <StyledButton disabled={isDisabled}>Get Help</StyledButton>
@@ -158,9 +194,31 @@ const PracticeBoard = () => {
                   </Col>
                 </Row>
               </QuestionContainer>
+              <div style={{ marginTop: "30px" }}>
+                {isGetHelp && (
+                  <QuestionContainer>
+                    <Text style={{ fontSize: "20px", fontWeight: 600 }}>Help:</Text>
+                    <div style={{ paddingTop: "15px" }}>
+                      <Text style={{ fontSize: "18px" }}>{help}</Text>
+                    </div>
+                  </QuestionContainer>
+                )}
+              </div>
             </Col>
             <Col span={8}>
-              <InstructionContainer></InstructionContainer>
+              <InstructionContainer>
+                <div style={{ paddingBottom: "10px" }}>
+                  <Text style={{ fontSize: "20px", fontWeight: 600 }}>Instructions:</Text>
+                </div>
+                {allQuestions &&
+                  allQuestions.data &&
+                  allQuestions.data.instructions.map((item: any, index: any) => (
+                    <div key={index} style={{ paddingBottom: "5px" }}>
+                      <Text style={{ fontSize: "17px" }}>{item}</Text>
+                    </div>
+                  ))}
+                <a>Watch Video</a>
+              </InstructionContainer>
             </Col>
           </Row>
         </BodyContainer>
